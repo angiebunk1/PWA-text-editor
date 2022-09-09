f33aaffc7db67cc1e605aee6839d84fe0f6c7e57
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-09 11:46:10 -0500

    made some edits

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,15 +4,19 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-installBtn.style.visibility = 'visible';
+butInstall.style.visibility = 'visible';
+
+butInstall.addEventListener('click', async () => {
+    event.prompt();
+butInstall.setAttribute('disabled', true);
+butInstall.textContent = 'Installed!';
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async (event) => {
-    event.prompt();
-installBtn.setAttribute('disabled', true);
-installBtn.textContent = 'Installed!';
+
 });
+
+
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
