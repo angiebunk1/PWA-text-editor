8016b8b2d1138a5245debe93ad362134b58c2542
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-09-10 14:10:04 -0500

    more testing done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log('PUT to the database');
 
   const jateDb = await openDB('jate', 1);
@@ -22,7 +22,7 @@ export const putDb = async (content) => {
 
   const store = tx.objectStore('jate');
   
-  const request = store.put({ content });
+  const request = store.put({ id:1, jate: content });
   const result = await request;
   console.log('data saved to the database', result);
 };
